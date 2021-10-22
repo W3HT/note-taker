8c46684fcd19f91f27b3628429893d4e03bacdbf
Frost <mpyang2021@gmail.com>
2021-10-21 20:55:47 -0500

    populate html routes

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,5 +1,23 @@
 // dependencies
+const path = require("path");
+const app = require("../../../projects/express/28-Stu_Mini-Project/Develop/routes");
+
 // routers
-    //notes
+module.exports = Function (app) {
+
+    // html get notes
+    app.get('/notes', function (res, req) {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
+
     // /
-    // *
+    app.get('/', function (res, req) {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+    // * 
+    app.get('*', function (res, req) {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+
+};
+ 
